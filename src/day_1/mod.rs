92b9b15bfdd680ba88cdd9cb7b1bf49b769bf1dc
Zane Duffield <duffieldzane@gmail.com>
2021-12-02 23:32:10 +1100

    solve day 1

---
+++ src/day_1/mod.rs
@@ -0,0 +1,48 @@
+pub fn run() {
+    let input = include_str!("input.txt");
+    println!(
+        "day 1\n  part 1:{}\n  part 2: {}\n",
+        part_1(input),
+        part_2(input)
+    );
+}
+
+fn part_1(contents: &str) -> usize {
+    count_window_increases(contents, 1)
+}
+
+fn part_2(contents: &str) -> usize {
+    count_window_increases(contents, 3)
+}
+
+fn count_window_increases(contents: &str, window_size: usize) -> usize {
+    contents
+        .lines()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect::<Vec<_>>()
+        .windows(window_size + 1)
+        .filter(|w| w.last() > w.first())
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::part_1;
+    use super::part_2;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(""), 0);
+        assert_eq!(part_1("1"), 0);
+        assert_eq!(part_1("1\n1\n2\n-1\n3"), 2);
+        assert_eq!(part_1("-1\n3\n3\n4\n10\n101\n101\n102"), 5);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(""), 0);
+        assert_eq!(part_2("1"), 0);
+        assert_eq!(part_2("1\n1\n2\n-1\n3"), 1);
+        assert_eq!(part_2("-1\n3\n3\n4\n10\n101\n101\n102"), 5);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod day_1;
+mod day_2;
+
+fn main() {
+    day_1::run();
+    day_2::run();
+}
