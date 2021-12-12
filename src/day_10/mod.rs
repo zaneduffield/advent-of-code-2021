d8b12c7a26c93a06d5b4abd94378248610ea29ef
Zane Duffield <duffieldzane@gmail.com>
2021-12-12 14:48:49 +1100

    solve day 10

---
+++ src/day_10/mod.rs
@@ -0,0 +1,91 @@
+use itertools::Itertools;
+
+struct ParseErr {
+    c: char,
+}
+
+fn parse_line(line: &str) -> Result<Vec<char>, ParseErr> {
+    let mut stack = Vec::with_capacity(line.len());
+    for c in line.chars() {
+        if Some(&c) == stack.last() {
+            stack.pop();
+            continue;
+        }
+        stack.push(match c {
+            '(' => ')',
+            '[' => ']',
+            '{' => '}',
+            '<' => '>',
+            _ => return Err(ParseErr { c }),
+        });
+    }
+
+    Ok(stack)
+}
+
+#[aoc(day10, part1)]
+fn part_1(input: &str) -> u64 {
+    input
+        .lines()
+        .map(parse_line)
+        .flat_map(Result::err)
+        .map(|ParseErr { c }| match c {
+            ')' => 3,
+            ']' => 57,
+            '}' => 1197,
+            '>' => 25137,
+            _ => panic!("Unexpected character {}", c),
+        })
+        .sum()
+}
+
+fn score_stack(stack: Vec<char>) -> u64 {
+    stack
+        .iter()
+        .rev()
+        .map(|&c| match c {
+            ')' => 1,
+            ']' => 2,
+            '}' => 3,
+            '>' => 4,
+            _ => panic!("Unexpected character {}", c),
+        })
+        .reduce(|score, c| score * 5 + c)
+        .unwrap_or(0)
+}
+
+#[aoc(day10, part2)]
+fn part_2(input: &str) -> u64 {
+    let scores: Vec<_> = input
+        .lines()
+        .map(parse_line)
+        .flat_map(Result::ok)
+        .map(score_stack)
+        .sorted_unstable()
+        .collect();
+
+    scores[scores.len() / 2]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let input = "\
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]";
+
+        assert_eq!(26397, part_1(input));
+        assert_eq!(288957, part_2(input));
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 extern crate aoc_runner_derive;
 
 pub mod day_1;
+pub mod day_10;
 pub mod day_2;
 pub mod day_3;
 pub mod day_4;
