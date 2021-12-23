cea0f99deb03d49c3b34fb7f7c4199ad6f67ccf1
Zane Duffield <duffieldzane@gmail.com>
2021-12-23 23:08:33 +1100

    solve day 18 faster

--- src/day_18/mod.rs
+++ src/day_18/mod.rs
@@ -1,159 +1,124 @@
-use std::{fmt, iter::Peekable};
+use std::iter::Peekable;
 
-use itertools::Itertools;
-
-#[derive(Debug, Clone)]
-enum SnailfishNum {
-    Literal(i32),
-    Pair(Box<SnailfishNum>, Box<SnailfishNum>),
+#[derive(Clone, Copy, Debug)]
+struct Entry {
+    depth: u8,
+    value: u8,
 }
 
-enum Side {
-    Left,
-    Right,
-}
-
-impl fmt::Display for SnailfishNum {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Literal(v) => write!(f, "{}", v),
-            Pair(left, right) => write!(f, "[{},{}]", left, right),
-        }
-    }
-}
-
-use SnailfishNum::*;
-
-type Residue = (i32, i32);
-
-impl SnailfishNum {
-    fn magnitude(&self) -> i32 {
-        match self {
-            Literal(v) => *v,
-            Pair(left, right) => 3 * left.magnitude() + 2 * right.magnitude(),
-        }
-    }
-
-    fn add(&self, other: &Self) -> Self {
-        Self::Pair(Box::new(self.clone()), Box::new(other.clone()))
-    }
-
-    fn reduce(mut self) -> Self {
-        loop {
-            if self.explode(0).is_none() && !self.split() {
-                break;
-            }
-        }
-        self
-    }
-
-    fn split(&mut self) -> bool {
-        match self {
-            Literal(val) if *val >= 10 => {
-                let (left, right) = (*val / 2, (*val + 1) / 2);
-                *self = Pair(Box::new(Literal(left)), Box::new(Literal(right)));
-                true
-            }
-            Pair(left, right) => left.split() || right.split(),
-            _ => false,
-        }
-    }
-
-    fn explode(&mut self, depth: u32) -> Option<Residue> {
-        if depth >= 4 {
-            let residue = self.residue();
-            if residue.is_some() {
-                *self = Literal(0);
-                return residue;
-            }
-        }
-
-        match self {
-            Pair(left, right) => {
-                if let Some(residue) = left.explode(depth + 1) {
-                    right.apply_residue(residue.1, Side::Left);
-                    Some((residue.0, 0))
-                } else if let Some(residue) = right.explode(depth + 1) {
-                    left.apply_residue(residue.0, Side::Right);
-                    Some((0, residue.1))
-                } else {
-                    None
-                }
-            }
-            Literal(_) => None,
-        }
-    }
-
-    fn value(&self) -> Option<i32> {
-        if let Literal(v) = self {
-            Some(*v)
-        } else {
-            None
-        }
-    }
-
-    fn residue(&self) -> Option<Residue> {
-        if let Pair(left, right) = self {
-            if let (Some(val_left), Some(val_right)) = (left.value(), right.value()) {
-                return Some((val_left, val_right));
-            }
-        }
-        None
-    }
-
-    fn apply_residue(&mut self, val: i32, side: Side) {
-        match self {
-            Literal(old) => {
-                *self = Literal(*old + val);
-            }
-            Pair(left, right) => match side {
-                Side::Left => left.apply_residue(val, side),
-                Side::Right => right.apply_residue(val, side),
-            },
-        }
-    }
-}
-
-fn parse_literal<I: Iterator<Item = char>>(iter: &mut Peekable<I>) -> SnailfishNum {
-    let num = iter
-        .peeking_take_while(|c| c.is_digit(10) || c == &'-')
-        .collect::<String>();
-    SnailfishNum::Literal(num.parse().unwrap())
-}
-
-fn parse_pair<I: Iterator<Item = char>>(iter: &mut Peekable<I>) -> SnailfishNum {
-    if Some(&'[') == iter.peek() {
-        iter.next();
-        let left = parse_pair(iter);
-        iter.next();
-        let right = parse_pair(iter);
-        iter.next();
-        SnailfishNum::Pair(Box::new(left), Box::new(right))
+fn val(entries: &mut Peekable<impl Iterator<Item = Entry>>, depth: u8) -> u32 {
+    let first = entries.peek().unwrap();
+    if first.depth == depth {
+        entries.next().unwrap().value as u32
     } else {
-        parse_literal(iter)
+        recursive_magnitude(entries, depth + 1)
     }
 }
 
-fn parse(input: &str) -> Vec<SnailfishNum> {
-    input
-        .lines()
-        .map(|line| parse_pair(&mut line.chars().peekable()))
-        .collect()
+fn recursive_magnitude<I>(entries: &mut Peekable<I>, depth: u8) -> u32
+where
+    I: Iterator<Item = Entry>,
+{
+    3 * val(entries, depth) + 2 * val(entries, depth)
+}
+
+fn magnitude(entries: Vec<Entry>) -> u32 {
+    recursive_magnitude(&mut entries.into_iter().peekable(), 1)
+}
+
+fn add(a: &[Entry], b: &[Entry]) -> Vec<Entry> {
+    let mut out = a.to_vec();
+    out.extend_from_slice(b);
+    out.iter_mut().for_each(|e| e.depth += 1);
+    out
+}
+
+fn reduce(mut entries: Vec<Entry>) -> Vec<Entry> {
+    loop {
+        if !explode(&mut entries) && !split(&mut entries) {
+            break;
+        }
+    }
+    entries
+}
+
+fn split(entries: &mut Vec<Entry>) -> bool {
+    match entries.iter().position(|e| e.value > 9) {
+        None => false,
+        Some(p) => {
+            let old = entries[p].value;
+            entries[p].value = old / 2;
+            entries[p].depth += 1;
+            entries.insert(
+                p + 1,
+                Entry {
+                    depth: entries[p].depth,
+                    value: (old + 1) / 2,
+                },
+            );
+            true
+        }
+    }
+}
+
+fn explode(entries: &mut Vec<Entry>) -> bool {
+    match entries.iter().position(|e| e.depth > 4) {
+        Some(p) if p < entries.len() - 1 => {
+            let (left, right) = (entries[p].value, entries[p + 1].value);
+            if p > 0 {
+                entries[p - 1].value += left;
+            }
+            if p < entries.len() - 2 {
+                entries[p + 2].value += right;
+            }
+            entries[p].value = 0;
+            entries[p].depth -= 1;
+            entries.remove(p + 1);
+            true
+        }
+        _ => false,
+    }
+}
+
+fn parse_num(num: &str) -> Vec<Entry> {
+    num.bytes()
+        .fold(
+            (0, Vec::with_capacity(num.len() / 2)),
+            |(mut depth, mut v), value| {
+                match value {
+                    b'[' => depth += 1,
+                    b']' => depth -= 1,
+                    b'0'..=b'9' => v.push(Entry {
+                        depth,
+                        value: value - b'0',
+                    }),
+                    _ => {}
+                }
+                (depth, v)
+            },
+        )
+        .1
+}
+
+fn parse(input: &str) -> Vec<Vec<Entry>> {
+    input.lines().map(parse_num).collect()
 }
 
 #[aoc(day18, part1)]
-fn part_1(input: &str) -> i32 {
+fn part_1(input: &str) -> u32 {
     let nums = parse(input);
-    let sum = nums.into_iter().reduce(|a, b| a.add(&b).reduce()).unwrap();
-    sum.magnitude()
+    let sum = nums.into_iter().reduce(|a, b| reduce(add(&a, &b))).unwrap();
+    magnitude(sum)
 }
 
 #[aoc(day18, part2)]
-fn part_2(input: &str) -> i32 {
+fn part_2(input: &str) -> u32 {
     let nums = parse(input);
     (0..nums.len())
         .flat_map(|i| (0..nums.len()).map(move |j| (i, j)))
-        .map(|(i, j)| nums[i].add(&nums[j]).reduce().magnitude())
+        .map(|(i, j)| add(&nums[i], &nums[j]))
+        .map(reduce)
+        .map(magnitude)
         .max()
         .unwrap_or(0)
 }
@@ -164,6 +129,11 @@ pub mod tests {
 
     #[test]
     pub fn test() {
+        let num = parse_num("[[1,2],[[3,4],5]]");
+        assert_eq!(143, magnitude(num));
+        let num = parse_num("[[[[5,0],[7,4]],[5,5]],[6,6]]");
+        assert_eq!(1137, magnitude(num));
+
         let input = "\
 [[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
 [[[5,[2,8]],4],[5,[[9,9],0]]]
